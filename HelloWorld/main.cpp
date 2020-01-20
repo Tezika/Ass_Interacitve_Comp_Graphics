4b0e838ea4ce6eb9381c5a726e0426e56dfebf83
Tezika <tezikazhou@gmail.com>
2020-01-19 22:47:29 -0700

    Changed the 3rd graphics library
    FreeGlut -> GLFW.

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,51 +1,37 @@
-#include <GL/freeglut.h>
-#include <iostream>
-#include <math.h>
+#include <GLFW/glfw3.h>
 
-void ProcessKeyboardKey( unsigned char i_key, int x, int y )
+int main( void )
 {
-	// 27 is the ACSII keycode for the 'esc' key.
-	if (i_key == 27)
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow( 640, 480, "Hello World", NULL, NULL );
+	if (!window)
 	{
-		exit( 0 );
+		glfwTerminate();
+		return -1;
 	}
-}
 
-void Display()
-{
-	glClear( GL_COLOR_BUFFER_BIT );
-	glutSwapBuffers();
-}
+	/* Make the window's context current */
+	glfwMakeContextCurrent( window );
 
-void Idle()
-{
-	// Submit the animated background colors
-	float elapsedTime = glutGet( GLUT_ELAPSED_TIME ) / 1000.0;
-	float r = 1 + sinf( elapsedTime ) / 2;
-	float g = 1 + cosf( elapsedTime ) / 2;
-	float b = 1 + cosf( elapsedTime ) / 2;
-	float a = 1 + cosf( elapsedTime ) / 2;
-	glClearColor( r, g, b, a );
-	glutPostRedisplay();
-}
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose( window ))
+	{
+		/* Render here */
+		glClear( GL_COLOR_BUFFER_BIT );
 
-void Init()
-{
-	glClearColor( 1.0, 1.0, 1.0, 1.0 );
-	glMatrixMode( GL_PROJECTION );
-	glLoadIdentity();
-	gluOrtho2D( 0.0, 499.0, 0.0, 499.0 );
-}
+		/* Swap front and back buffers */
+		glfwSwapBuffers( window );
 
-void main( int argc, char** argv ) {
-	glutInit( &argc, argv );
-	glutInitDisplayMode( GLUT_SINGLE | GLUT_RGB );
-	glutInitWindowSize( 500, 500 );
-	glutInitWindowPosition( 0, 0 );
-	glutCreateWindow( "HelloWorld" );
-	glutDisplayFunc( Display );
-	glutKeyboardFunc( ProcessKeyboardKey );
-	glutIdleFunc( Idle );
-	Init();
-	glutMainLoop();
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
