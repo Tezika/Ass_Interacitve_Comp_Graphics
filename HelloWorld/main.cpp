f6ad4aeea19a0d37aa8372ba9da0f23602131ac5
Tezikazhou <tezikazhou@gmail.com>
2020-01-27 10:07:19 -0700

    Added the teapot .obj file.

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -27,9 +27,9 @@ cyMatrix4f g_mat_perspective;
 void InitializeShaders()
 {
 	g_shaderProgram.CreateProgram();
-	if (!g_vertexShader.CompileFile("content/vertex.shader", GL_VERTEX_SHADER))
+	if (!g_vertexShader.CompileFile( "content/vertex.shader", GL_VERTEX_SHADER ))
 	{
-		fprintf( stderr, "Failed to compile the vertex shader.\n");
+		fprintf( stderr, "Failed to compile the vertex shader.\n" );
 	}
 	else
 	{
@@ -51,7 +51,7 @@ void InitializeShaders()
 
 void InitializeGL()
 {
-	if (!g_triMesh.LoadFromFileObj( "content/teapot.obj" ))
+	if (!g_triMesh.LoadFromFileObj( "content/teapot.objt" ))
 	{
 		fprintf( stderr, "Failed to load the teapot.obj.\n" );
 		assert( false );
@@ -91,7 +91,7 @@ void InitializeGL()
 	}
 	glBufferData( GL_ELEMENT_ARRAY_BUFFER, static_cast <GLsizeiptr>(sizeof( unsigned int ) * 3 * g_triMesh.NF()), reinterpret_cast<void*>(&indices[0]), GL_STATIC_DRAW );
 	assert( glGetError() == GL_NO_ERROR );
-	
+
 	glBindBuffer( GL_ARRAY_BUFFER, 0 );
 	glBindVertexArray( 0 );
 }
@@ -155,9 +155,9 @@ void UpdateView()
 	g_mat_view = cyMatrix4f( 1.0f );
 	g_mat_perspective = cyMatrix4f( 1.0f );
 
-	g_mat_model.SetRotationX( glm::radians(-60.0f) );
+	g_mat_model.SetRotationX( glm::radians( -60.0f ) );
 	g_mat_view.SetTranslation( cyVec3f( 0, 0, -50.0f ) );
-	g_mat_perspective.SetPerspective( glm::radians(45.0f), 640.0f / 480.0f, 0.1f, 100.0f );
+	g_mat_perspective.SetPerspective( glm::radians( 45.0f ), 640.0f / 480.0f, 0.1f, 100.0f );
 
 	unsigned int modelLoc = glGetUniformLocation( g_shaderProgram.GetID(), "model" );
 	glUniformMatrix4fv( modelLoc, 1, GL_FALSE, g_mat_model.cell );
@@ -218,6 +218,9 @@ int main( void )
 		assert( glGetError() == GL_NO_ERROR );
 		glDeleteBuffers( 1, &VBO );
 		assert( glGetError() == GL_NO_ERROR );
+		glDeleteBuffers( 1, &EBO );
+		assert( glGetError() == GL_NO_ERROR );
+
 		g_vertexShader.Delete();
 		g_fragmentShader.Delete();
 		g_shaderProgram.Delete();
